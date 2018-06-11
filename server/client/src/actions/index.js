da84c0e5695c7d04d5d2872edc423a5b6816e7f7
Jorge E Sosa <jorgeduardo.1994@gmail.com>
2018-06-11 18:36:41 -0500

    added actions, and reducers, connected app with backend server.

---
+++ server/client/src/actions/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// action types
+import { FETCH_USER } from "./types.js";
+
+const fetchUser = () => async dispatch => {
+	const res = await axios.get("/api/current_user");
+	dispatch({
+		type: FETCH_USER,
+		payload: res.data
+	});
+};
+
+// above function before refactoring
+// const fetchUser = () => {
+// 	return async function(dispatch) {
+// 		const res = await axios.get("/api/current_user");
+// 		dispatch({
+// 			type: FETCH_USER,
+// 			payload: res
+// 		});
+// 	};
+// };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,14 +1,16 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 //components
 import App from "./components/App";
 import reducers from "./reducers/";
 
 //first argument to createStore are all the reducers
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}>
